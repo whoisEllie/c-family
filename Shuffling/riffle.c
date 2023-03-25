3552bca1283268aa265f6c6f51038644154c616e
Ellie Kelemen <37041249+whoisEllie@users.noreply.github.com>
2023-03-25 14:19:16 +0000

    working riffle

--- Shuffling/riffle.c
+++ Shuffling/riffle.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 void riffle_once(void *L, int len, int size, void *work)
 {
@@ -10,13 +11,39 @@ void riffle_once(void *L, int len, int size, void *work)
 	work = malloc(sizeof(L));
 
 	// Obtaining pointers to the head of our input arrays, B will be incremented after a midway point is calculated
-	void *A = L;
-	void *B = L;
-	void *out = work;
+	char *A = L;
+	char *B = L;
+	int aCount = 0; 
+	int bCount = 0;
+	char *out = work;
 
 	int midPoint = ceil((double)len/2);	
 	B = B + midPoint * size;
 
+	for (int i = 0; i < len; i++) {
+		if((rand()%2 && bCount < (len - midPoint)) || aCount >= midPoint)
+		{
+			printf("Updating B to %d\n", *(int*)B);
+			*out = *B; 
+			out += size;
+			B += size;
+			bCount++;
+		}
+		else
+		{
+			printf("Updating A to %d\n", *(int*)A);
+			*out = *A;
+			out += size;
+			A += size;
+			aCount++;
+		}
+	}
+	
+	void* C = work;
+	for (int j = 0; j < len; j++) {
+		printf("%d ", *(int*)C);
+		C += size;
+	}
 }
 
 int main(int argv, char* argc[])
@@ -25,6 +52,8 @@ int main(int argv, char* argc[])
 	int testint[6] = {1, 2, 3, 4, 5, 6};
 	char testchar[6] = {'a', 'b', 'c', 'd', 'e', 'f'};
 
+
+	srand(time(NULL));
 	printf("Running int riffle\n");
 	riffle_once(testint, 6, sizeof(int), work);
 }
